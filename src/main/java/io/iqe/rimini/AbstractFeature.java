ef4aa5fc08aff6e28754cb0eb7a222992e304f65
Michael Goth <iqe@gmx.net>
2014-06-19 20:54:53 +0200

    Mark AbstractFeature as abstract (ahem...)

--- src/main/java/io/iqe/rimini/AbstractFeature.java
+++ src/main/java/io/iqe/rimini/AbstractFeature.java
@@ -2,7 +2,7 @@ package io.iqe.rimini;
 
 import io.iqe.nio.MultiSignByteBuffer;
 
-public class AbstractFeature<C> implements Feature<C> {
+public abstract class AbstractFeature<C> implements Feature<C> {
     @Override
     public void writeMessageContent(C content, MultiSignByteBuffer buf) {
         throw new UnsupportedOperationException(getClass().getSimpleName() + " does not support writing messages");
