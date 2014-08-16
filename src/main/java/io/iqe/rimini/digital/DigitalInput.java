76c25da4006973b07cc304165463f19b5d4cd18f
Michael Goth <iqe@gmx.net>
2014-08-16 20:59:50 +0200

    Quick'n'dirty implementation of DigitalInputConfig

--- src/main/java/io/iqe/rimini/digital/DigitalInput.java
+++ src/main/java/io/iqe/rimini/digital/DigitalInput.java
@@ -8,4 +8,34 @@ public class DigitalInput extends AbstractFeature<Boolean> {
     public Boolean readMessageContent(MultiSignByteBuffer buf) {
         return buf.getUnsigned() == 'H';
     }
+
+    @Override
+    public Object readConfiguration(MultiSignByteBuffer buf) {
+        buf.getUnsigned(); // skip pin count
+
+        int pin = buf.getUnsigned();
+        int flags = buf.getUnsigned();
+        long debounceMillis = buf.getUnsignedInt();
+
+        boolean usingInputPullup = (flags & 1) == 1;
+        boolean reversed = (flags & 2) == 2;
+
+        return new DigitalInputConfig(pin, usingInputPullup, reversed, debounceMillis);
+    }
+
+    @Override
+    public void writeConfiguration(Object configuration, MultiSignByteBuffer buf) {
+        if (!(configuration instanceof DigitalInputConfig)) {
+            throw new IllegalArgumentException(); // FIXME
+        }
+
+        DigitalInputConfig config = (DigitalInputConfig) configuration;
+
+        int flags = (config.isUsingInputPullup() ? 1 : 0) + (config.isReversed() ? 2 : 0);
+
+        buf.putUnsigned(1);
+        buf.putUnsigned(config.getPin());
+        buf.putUnsigned(flags);
+        buf.putUnsignedInt(config.getDebounceMillis());
+    }
 }
--- src/main/java/io/iqe/rimini/digital/DigitalInputConfig.java
+++ src/main/java/io/iqe/rimini/digital/DigitalInputConfig.java
@@ -1,14 +1,25 @@
 package io.iqe.rimini.digital;
 
-public class DigitalInputConfig {
+import java.util.Arrays;
+
+import io.iqe.rimini.FeatureType;
+import io.iqe.rimini.config.FeatureConfiguration;
+
+public class DigitalInputConfig extends FeatureConfiguration {
     private boolean usingInputPullup;
     private boolean reversed;
     private long debounceMillis;
 
-    public DigitalInputConfig(boolean usingInputPullup, boolean reversed, long debounceMillis) {
+    public DigitalInputConfig(int pin, boolean usingInputPullup, boolean reversed, long debounceMillis) {
+        super(FeatureType.DIGITAL_INPUT, Arrays.asList(pin));
+
         this.usingInputPullup = usingInputPullup;
         this.reversed = reversed;
         this.debounceMillis = debounceMillis;
+    }
+
+    public int getPin() {
+        return getPins().get(0);
     }
 
     public boolean isUsingInputPullup() {
@@ -21,5 +32,11 @@ public class DigitalInputConfig {
 
     public long getDebounceMillis() {
         return debounceMillis;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s: Pin %d, Pullup %s, Reversed %s, Debounce %d", super.toString(),
+                getPin(), usingInputPullup, reversed, debounceMillis);
     }
 }
